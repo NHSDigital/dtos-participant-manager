565d734464f9d9c761b32a1f46d6148a71db3b76
Adam Chrimes <iChrimes@live.co.uk>
2025-02-10 17:11:45 +0000

    feat: provide the account access token in the session to pass into the API

--- src/web/app/lib/fetchPatientData.ts
+++ src/web/app/lib/fetchPatientData.ts
@@ -1,8 +1,28 @@
 export async function fetchPatientData(nhsNumber: string) {
   try {
     const url = `${process.env.API_URL}/api/participants?nhsnumber=${nhsNumber}`;
-    console.log(url);
     const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
+
+export async function fetchPatientScreeningEligibility(accessToken: string) {
+  try {
+    const url = `${process.env.EXPERIENCE_PORT}/api/GetScreeningEligibility`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`Error fetching data: ${response.statusText}`);
