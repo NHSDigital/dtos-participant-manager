b769ec0cc54072a490660da83793ad0ff55f3c27
Adam Chrimes <iChrimes@live.co.uk>
2025-02-19 10:51:44 +0000

    feat: setup basic BDD playwright tests

--- src/web/playwright.config.ts
+++ src/web/playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import { defineBddConfig } from "playwright-bdd";
 
 /**
  * Read environment variables from file.
@@ -9,10 +10,18 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env.tests") });
 
 /**
+ * Define BDD configuration.
+ */
+const testDir = defineBddConfig({
+  features: "./e2e/features",
+  steps: "./e2e/steps",
+});
+
+/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./e2e",
+  testDir,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,12 +33,12 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    baseURL: "http://localhost:3000",
+  // use: {
+  //   baseURL: "http://localhost:3000",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-  },
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: "on-first-retry",
+  // },
 
   /* Configure projects for major browsers */
   projects: [
@@ -67,9 +76,9 @@ export default defineConfig({
     // },
   ],
 
-  webServer: {
-    command: "pnpm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "pnpm run dev",
+  //   url: "http://localhost:3000",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
