5136cbeef556ad4792c7be1d78aed62d14d524b4
Adam Chrimes <iChrimes@live.co.uk>
2025-02-20 13:57:33 +0000

    feat: use the breadcrumb component instead of back link

--- src/web/app/cervical-screening/[assignmentId]/page.tsx
+++ src/web/app/cervical-screening/[assignmentId]/page.tsx
@@ -3,7 +3,7 @@ import type { Session } from "next-auth";
 import type { PathwayItem } from "@/app/types";
 import { auth } from "@/app/lib/auth";
 import { fetchPathwayAssignment } from "@/app/lib/fetchPatientData";
-import BackLink from "@/app/components/backLink";
+import Breadcrumb from "@/app/components/breadcrumb";
 import Card from "@/app/components/card";
 import InsetText from "@/app/components/insetText";
 import SignOutButton from "@/app/components/signOutButton";
@@ -47,13 +47,14 @@ export default async function Page(props: {
 
   if (!session) return <Unauthorised />;
 
+  const breadcrumbItems = [{ label: "Home", url: "/" }];
   const params = await props.params;
   const assignmentId = params.assignmentId;
   const eligibility = await getPathwayAssignment(session, assignmentId);
 
   return (
     <>
-      <BackLink url="/" />
+      <Breadcrumb items={breadcrumbItems} />
       <main className="nhsuk-main-wrapper" id="maincontent" role="main">
         <div className="nhsuk-grid-row">
           <div className="nhsuk-grid-column-two-thirds">
