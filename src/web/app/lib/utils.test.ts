64928d1cd7320c7f24389adde6000de9ae9fb34b
Adam Chrimes <iChrimes@live.co.uk>
2025-03-05 10:25:56 +0000

    chore: remove unused utility functions

--- src/web/app/lib/utils.test.ts
+++ src/web/app/lib/utils.test.ts
@@ -1,23 +1,4 @@
-import {
-  formatNhsNumber,
-  formatDate,
-  formatCompactDate,
-  createUrlSlug,
-} from "@/app/lib/utils";
-
-describe("formatNhsNumber", () => {
-  it("should format the NHS number as XXX XXX XXXX", () => {
-    const input = "1234567890";
-    const expectedOutput = "123 456 7890";
-    expect(formatNhsNumber(input)).toBe(expectedOutput);
-  });
-
-  it("should return the input if it is not a valid NHS number", () => {
-    const input = "12345";
-    const expectedOutput = "12345";
-    expect(formatNhsNumber(input)).toBe(expectedOutput);
-  });
-});
+import { formatDate, createUrlSlug } from "@/app/lib/utils";
 
 describe("formatDate", () => {
   it("should format the date as 26 February 1993", () => {
@@ -27,20 +8,9 @@ describe("formatDate", () => {
   });
 });
 
-describe("formatCompactDate", () => {
-  it("should format the date as 26 February 1993", () => {
-    const input = "19930226";
-    const expectedOutput = "26 February 1993";
-    expect(formatCompactDate(input)).toBe(expectedOutput);
-  });
-});
-
 describe("createUrlSlug", () => {
   it("converts string to url friendly slug", () => {
     expect(createUrlSlug("Breast Screening")).toBe("breast-screening");
-    expect(createUrlSlug("Bowel & Cancer Screening")).toBe(
-      "bowel-cancer-screening"
-    );
-    expect(createUrlSlug("  Multiple   Spaces  ")).toBe("multiple-spaces");
+    expect(createUrlSlug("prostate cancer")).toBe("prostate-cancer");
   });
 });
