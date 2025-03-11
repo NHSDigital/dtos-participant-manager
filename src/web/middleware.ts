8c3d264b4af2eaf80832f49f0356029fccd50359
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-03-11 11:29:29 +0000

    feat: Attempting to sign out when RefreshTokenError present

--- src/web/middleware.ts
+++ src/web/middleware.ts
@@ -2,11 +2,14 @@ import { NextResponse, NextRequest } from "next/server";
 import { getAuthConfig } from "@/app/lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const { auth } = await getAuthConfig();
+  const { auth, signOut } = await getAuthConfig();
   const session = await auth();
 
   if (!session) {
     return NextResponse.redirect(new URL("/access-denied", request.url));
+  }
+  if (session?.error === "RefreshTokenError") {
+    return signOut({ redirectTo: "/" });
   }
 
   return NextResponse.next();
