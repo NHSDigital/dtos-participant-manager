f330ae9d647c672c1176479896bdf525d0936698
Adam Chrimes <iChrimes@live.co.uk>
2025-03-18 11:52:12 +0000

    chore: add basic error log handling for the catch all error page

--- src/web/app/error/page.tsx
+++ src/web/app/error/page.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
+import { logger } from "@/app/lib/logger";
 
 export const metadata: Metadata = {
   title: `There’s a problem with the service - ${process.env.SERVICE_NAME} - NHS`,
 };
 
-export default async function Page() {
+export default async function Page(error: Error) {
+  if (error) {
+    logger.error("Application error:", {
+      message: error.message,
+      stack: error.stack,
+      name: error.name,
+    });
+  }
   return (
     <main className="nhsuk-main-wrapper" id="maincontent" role="main">
       <div className="nhsuk-grid-row">
