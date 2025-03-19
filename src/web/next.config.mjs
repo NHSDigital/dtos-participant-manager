d409d93d0dd28576e39e82bacada49c7a0ed4968
Adam Chrimes <iChrimes@live.co.uk>
2025-03-19 10:15:19 +0000

    chore: update to latest versions of dependencies

--- src/web/next.config.mjs
+++ src/web/next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "standalone",
   serverExternalPackages: ["pino", "pino-pretty"],
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
 export default nextConfig;
