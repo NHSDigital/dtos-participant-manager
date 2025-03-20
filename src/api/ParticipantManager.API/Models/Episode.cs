276909472f66931ed8e1e22bcbc3a0051edabeec
Tyrrellion <samuel.tyrell1@nhs.net>
2025-03-20 15:27:59 +0000

    fix: making model properties nullable for now

--- src/api/ParticipantManager.API/Models/Episode.cs
+++ src/api/ParticipantManager.API/Models/Episode.cs
@@ -4,8 +4,8 @@ public class Episode
 {
   public Guid EpisodeId { get; set; }
   public Guid EnrolmentId { get; set; }
-  public string PathwayVersion { get; set; }
-  public string Status { get; set; }
-  public PathwayTypeEnrolment PathwayTypeEnrolment { get; set; }
-  public ICollection<Encounter> Encounters { get; set; }
+  public string? PathwayVersion { get; set; }
+  public string? Status { get; set; }
+  public PathwayTypeEnrolment? PathwayTypeEnrolment { get; set; }
+  public ICollection<Encounter>? Encounters { get; set; }
 }
