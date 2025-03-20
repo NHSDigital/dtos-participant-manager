38ed2b149e8147a18c164464c597003fa4bbe7a0
warren <warren.pitterson1@nhs.net>
2025-03-20 16:26:40 +0000

    fix: .cs indent size revert back to 2, format reverted on experience functions and tests

--- src/api/ParticipantManager.Experience.API/Functions/ScreeningEligibilityFunction.cs
+++ src/api/ParticipantManager.Experience.API/Functions/ScreeningEligibilityFunction.cs
@@ -1,4 +1,3 @@
-using Google.Protobuf.WellKnownTypes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -15,72 +14,72 @@ public class ScreeningEligibilityFunction(
     ITokenService tokenService,
     IFeatureFlagClient featureFlagClient)
 {
-    [Function("GetScreeningEligibility")]
-    public async Task<IActionResult> GetParticipantEligibility(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "participant/{participantId}/eligibility")]
+  [Function("GetScreeningEligibility")]
+  public async Task<IActionResult> GetParticipantEligibility(
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "participant/{participantId}/eligibility")]
             HttpRequestData req, Guid participantId)
+  {
+    try
     {
-        try
+      var result = await tokenService.ValidateToken(req);
+
+      if (result.Status != AccessTokenStatus.Valid)
+      {
+        logger.LogError("Invalid access token");
+        return new UnauthorizedResult();
+      }
+
+      var nhsNumber = result.Principal.Claims.FirstOrDefault(c => c.Type == "nhs_number")?.Value;
+      if (string.IsNullOrEmpty(nhsNumber))
+      {
+        logger.LogError("Access token doesn't contain NHS number");
+        return new UnauthorizedResult();
+      }
+
+      if (participantId == Guid.Empty)
+      {
+        logger.LogError("Access token doesn't contain ParticipantId");
+        return new UnauthorizedResult();
+      }
+
+      var pathwayEnrolments = await crudApiClient.GetPathwayEnrolmentsAsync(participantId);
+      if (pathwayEnrolments == null)
+      {
+        logger.LogError("Failed to find pathway enrolments for Participant: {@ParticipantId}", new { ParticipantId = participantId });
+        return new NotFoundObjectResult("Unable to find pathway enrolments");
+      }
+
+      if (pathwayEnrolments.Count == 0)
+      {
+        logger.LogInformation("Successful response but no Pathway enrolments were found for Participant: {@ParticipantId}", new { ParticipantId = participantId });
+        return new OkObjectResult(new
         {
-            var result = await tokenService.ValidateToken(req);
+          Message = $"No pathway enrollments found for the participant: {participantId}",
+          Enrollments = new List<PathwayEnrolmentDto>()
+        });
+      }
 
-            if (result.Status != AccessTokenStatus.Valid)
-            {
-                logger.LogError("Invalid access token");
-                return new UnauthorizedResult();
-            }
+      //Check that logged in user has access to participant
+      if (pathwayEnrolments.Any(pathwayEnrolments => pathwayEnrolments.Participant?.NhsNumber != nhsNumber.ToString()))
+      {
+        logger.LogError("Logged in user does not have access to this record: {@ParticipantId}", new { ParticipantId = participantId });
+        return new UnauthorizedResult();
+      }
 
-            var nhsNumber = result.Principal.Claims.FirstOrDefault(c => c.Type == "nhs_number")?.Value;
-            if (string.IsNullOrEmpty(nhsNumber))
-            {
-                logger.LogError("Access token doesn't contain NHS number");
-                return new UnauthorizedResult();
-            }
+      var enabled = await featureFlagClient.IsFeatureEnabledForParticipant("mays_mvp", participantId);
 
-            if (participantId == Guid.Empty)
-            {
-                logger.LogError("Access token doesn't contain ParticipantId");
-                return new UnauthorizedResult();
-            }
+      if (!enabled)
+      {
+        return new ForbidResult();
+      }
 
-            var pathwayEnrolments = await crudApiClient.GetPathwayEnrolmentsAsync(participantId);
-            if (pathwayEnrolments == null)
-            {
-                logger.LogError("Failed to find pathway enrolments for Participant: {@ParticipantId}", new { ParticipantId = participantId });
-                return new NotFoundObjectResult("Unable to find pathway enrolments");
-            }
-
-            if (pathwayEnrolments.Count == 0)
-            {
-                logger.LogInformation("Successful response but no Pathway enrolments were found for Participant: {@ParticipantId}", new { ParticipantId = participantId });
-                return new OkObjectResult(new
-                {
-                    Message = $"No pathway enrollments found for the participant: {participantId}",
-                    Enrollments = new List<PathwayEnrolmentDto>()
-                });
-            }
-
-            //Check that logged in user has access to participant
-            if (pathwayEnrolments.Any(pathwayEnrolments => pathwayEnrolments.Participant?.NhsNumber != nhsNumber.ToString()))
-            {
-                logger.LogError("Logged in user does not have access to this record: {@ParticipantId}", new { ParticipantId = participantId });
-                return new UnauthorizedResult();
-            }
-
-            var enabled = await featureFlagClient.IsFeatureEnabledForParticipant("mays_mvp", participantId);
-
-            if (!enabled)
-            {
-                return new ForbidResult();
-            }
-
-            logger.LogInformation("Found pathway enrolments for NhsNumber: {@NhsNumber}", new { NhsNumber = nhsNumber });
-            return new OkObjectResult(pathwayEnrolments);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Invalid: Bad Request");
-            return new BadRequestObjectResult(ex.Message);
-        }
+      logger.LogInformation("Found pathway enrolments for NhsNumber: {@NhsNumber}", new { NhsNumber = nhsNumber });
+      return new OkObjectResult(pathwayEnrolments);
     }
+    catch (Exception ex)
+    {
+      logger.LogError(ex, "Invalid: Bad Request");
+      return new BadRequestObjectResult(ex.Message);
+    }
+  }
 }
--- tests/ParticipantManager.Experience.API.Tests/GetParticipantIdFunctionTests.cs
+++ tests/ParticipantManager.Experience.API.Tests/GetParticipantIdFunctionTests.cs
@@ -14,171 +14,171 @@ using ParticipantManager.Shared.DTOs;
 
 public class GetParticipantIdFunctionTests
 {
-    private readonly Mock<ICrudApiClient> _crudApiClient = new();
-    private readonly GetParticipantIdFunction _function;
-    private readonly Mock<ILogger<GetParticipantIdFunction>> _loggerMock;
-    private readonly Mock<ITokenService> _mockTokenService = new();
-    private readonly Mock<IFeatureFlagClient> _mockFeatureFlagClient = new();
-    private readonly Guid _validParticipantId = Guid.NewGuid();
-    private readonly HttpRequestData _request = CreateHttpRequest("");
+  private readonly Mock<ICrudApiClient> _crudApiClient = new();
+  private readonly GetParticipantIdFunction _function;
+  private readonly Mock<ILogger<GetParticipantIdFunction>> _loggerMock;
+  private readonly Mock<ITokenService> _mockTokenService = new();
+  private readonly Mock<IFeatureFlagClient> _mockFeatureFlagClient = new();
+  private readonly Guid _validParticipantId = Guid.NewGuid();
+  private readonly HttpRequestData _request = CreateHttpRequest("");
 
 
-    public GetParticipantIdFunctionTests()
-    {
-        _loggerMock = new Mock<ILogger<GetParticipantIdFunction>>();
-        _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync(It.IsAny<string>())).ReturnsAsync(MockParticipant);
-        _function = new GetParticipantIdFunction(_loggerMock.Object, _crudApiClient.Object, _mockTokenService.Object, _mockFeatureFlagClient.Object);
-    }
+  public GetParticipantIdFunctionTests()
+  {
+    _loggerMock = new Mock<ILogger<GetParticipantIdFunction>>();
+    _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync(It.IsAny<string>())).ReturnsAsync(MockParticipant);
+    _function = new GetParticipantIdFunction(_loggerMock.Object, _crudApiClient.Object, _mockTokenService.Object, _mockFeatureFlagClient.Object);
+  }
 
-    [Fact]
-    public async Task GetParticipantId_ShouldReturnUnauthorized_IfInvalidToken()
-    {
-        // Arrange
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Expired());
+  [Fact]
+  public async Task GetParticipantId_ShouldReturnUnauthorized_IfInvalidToken()
+  {
+    // Arrange
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Expired());
 
-        // Act
-        var response = await _function.GetParticipantId(_request) as UnauthorizedResult;
+    // Act
+    var response = await _function.GetParticipantId(_request) as UnauthorizedResult;
 
-        // Assert
-        Assert.Equal(StatusCodes.Status401Unauthorized, response?.StatusCode);
-    }
+    // Assert
+    Assert.Equal(StatusCodes.Status401Unauthorized, response?.StatusCode);
+  }
 
-    [Fact]
-    public async Task GetParticipantId__NoNhsNumber_ShouldReturnUnauthorized()
-    {
-        // Arrange
-        var claims = new List<Claim>
+  [Fact]
+  public async Task GetParticipantId__NoNhsNumber_ShouldReturnUnauthorized()
+  {
+    // Arrange
+    var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, "12345"),
             new(ClaimTypes.Email, "user@example.com"),
             new("custom_claim", "my_value")
         };
-        var identity = new ClaimsIdentity(claims, "Bearer");
-        var principal = new ClaimsPrincipal(identity);
+    var identity = new ClaimsIdentity(claims, "Bearer");
+    var principal = new ClaimsPrincipal(identity);
 
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
 
-        // Act
-        var response = await _function.GetParticipantId(_request) as UnauthorizedResult;
+    // Act
+    var response = await _function.GetParticipantId(_request) as UnauthorizedResult;
 
-        // Assert
-        Assert.Equal(StatusCodes.Status401Unauthorized, response?.StatusCode);
-    }
+    // Assert
+    Assert.Equal(StatusCodes.Status401Unauthorized, response?.StatusCode);
+  }
 
-    [Fact]
-    public async Task GetParticipantId_ParticipantIsNull_ReturnsNotFound()
-    {
-        // Arrange
-        var claims = new List<Claim>
+  [Fact]
+  public async Task GetParticipantId_ParticipantIsNull_ReturnsNotFound()
+  {
+    // Arrange
+    var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, "12345"),
             new(ClaimTypes.Email, "user@example.com"),
             new("nhs_number", "12345678")
         };
-        var identity = new ClaimsIdentity(claims, "Bearer");
-        var principal = new ClaimsPrincipal(identity);
+    var identity = new ClaimsIdentity(claims, "Bearer");
+    var principal = new ClaimsPrincipal(identity);
 
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
-        _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync("12345678")).Returns(Task.FromResult<ParticipantDto?>(null));
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
+    _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync("12345678")).Returns(Task.FromResult<ParticipantDto?>(null));
 
-        // Act
-        var response = await _function.GetParticipantId(_request) as NotFoundObjectResult;
+    // Act
+    var response = await _function.GetParticipantId(_request) as NotFoundObjectResult;
 
-        // Assert
-        Assert.Equal(StatusCodes.Status404NotFound, response?.StatusCode);
-        Assert.Equal("Unable to find participant", response?.Value);
-    }
+    // Assert
+    Assert.Equal(StatusCodes.Status404NotFound, response?.StatusCode);
+    Assert.Equal("Unable to find participant", response?.Value);
+  }
 
-    [Fact]
-    public async Task GetParticipantId_FeatureToggleDisabled_ReturnsForbidden()
-    {
-        // Arrange
-        var claims = new List<Claim>
+  [Fact]
+  public async Task GetParticipantId_FeatureToggleDisabled_ReturnsForbidden()
+  {
+    // Arrange
+    var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, "12345678"),
             new(ClaimTypes.Email, "user@example.com"),
             new("nhs_number", "12345678")
         };
-        var identity = new ClaimsIdentity(claims, "Bearer");
-        var principal = new ClaimsPrincipal(identity);
+    var identity = new ClaimsIdentity(claims, "Bearer");
+    var principal = new ClaimsPrincipal(identity);
 
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
-        _mockFeatureFlagClient.Setup(f => f.IsFeatureEnabledForParticipant("mays_mvp", _validParticipantId)).ReturnsAsync(false);
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
+    _mockFeatureFlagClient.Setup(f => f.IsFeatureEnabledForParticipant("mays_mvp", _validParticipantId)).ReturnsAsync(false);
 
-        // Act
-        var response = await _function.GetParticipantId(_request);
+    // Act
+    var response = await _function.GetParticipantId(_request);
 
-        // Assert
-        Assert.NotNull(response);
-        Assert.IsType<ForbidResult>(response);
-    }
+    // Assert
+    Assert.NotNull(response);
+    Assert.IsType<ForbidResult>(response);
+  }
 
-    [Fact]
-    public async Task GetParticipantId_WithValidToken_ReturnsOk()
-    {
-        // Arrange
-        var claims = new List<Claim>
+  [Fact]
+  public async Task GetParticipantId_WithValidToken_ReturnsOk()
+  {
+    // Arrange
+    var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, "12345678"),
             new(ClaimTypes.Email, "user@example.com"),
             new("nhs_number", "12345678")
         };
-        var identity = new ClaimsIdentity(claims, "Bearer");
-        var principal = new ClaimsPrincipal(identity);
+    var identity = new ClaimsIdentity(claims, "Bearer");
+    var principal = new ClaimsPrincipal(identity);
 
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
-        _mockFeatureFlagClient.Setup(f => f.IsFeatureEnabledForParticipant("mays_mvp", _validParticipantId)).ReturnsAsync(true);
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
+    _mockFeatureFlagClient.Setup(f => f.IsFeatureEnabledForParticipant("mays_mvp", _validParticipantId)).ReturnsAsync(true);
 
-        // Act
-        var response = await _function.GetParticipantId(_request) as OkObjectResult;
+    // Act
+    var response = await _function.GetParticipantId(_request) as OkObjectResult;
 
-        // Assert
-        Assert.Equal(StatusCodes.Status200OK, response?.StatusCode);
-        Assert.Equal(_validParticipantId, response?.Value);
-    }
+    // Assert
+    Assert.Equal(StatusCodes.Status200OK, response?.StatusCode);
+    Assert.Equal(_validParticipantId, response?.Value);
+  }
 
-    [Fact]
-    public async Task GetParticipantId_ExceptionThrown_ReturnBadRequest()
-    {
-        // Arrange
-        var claims = new List<Claim>
+  [Fact]
+  public async Task GetParticipantId_ExceptionThrown_ReturnBadRequest()
+  {
+    // Arrange
+    var claims = new List<Claim>
         {
             new(ClaimTypes.NameIdentifier, "12345678"),
             new(ClaimTypes.Email, "user@example.com"),
             new("nhs_number", "12345678")
         };
-        var identity = new ClaimsIdentity(claims, "Bearer");
-        var principal = new ClaimsPrincipal(identity);
+    var identity = new ClaimsIdentity(claims, "Bearer");
+    var principal = new ClaimsPrincipal(identity);
 
-        _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
-        _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync("12345678")).ThrowsAsync(new Exception("Test exception message"));
+    _mockTokenService.Setup(s => s.ValidateToken(It.IsAny<HttpRequestData>())).ReturnsAsync(AccessTokenResult.Success(principal));
+    _crudApiClient.Setup(s => s.GetParticipantByNhsNumberAsync("12345678")).ThrowsAsync(new Exception("Test exception message"));
 
-        // Act
-        var response = await _function.GetParticipantId(_request) as BadRequestObjectResult;
+    // Act
+    var response = await _function.GetParticipantId(_request) as BadRequestObjectResult;
 
-        // Assert
-        Assert.Equal(StatusCodes.Status400BadRequest, response?.StatusCode);
-        Assert.Equal("Test exception message", response?.Value);
-    }
+    // Assert
+    Assert.Equal(StatusCodes.Status400BadRequest, response?.StatusCode);
+    Assert.Equal("Test exception message", response?.Value);
+  }
 
-    private static HttpRequestData CreateHttpRequest(string? authHeader)
+  private static HttpRequestData CreateHttpRequest(string? authHeader)
+  {
+    var context = new Mock<FunctionContext>();
+    var request = new Mock<HttpRequestData>(MockBehavior.Strict, context.Object);
+    var headers = new HttpHeadersCollection(new List<KeyValuePair<string, string>>());
+    if (!string.IsNullOrEmpty(authHeader)) headers.Add("Authorization", $"{authHeader}");
+
+    request.Setup(r => r.Headers).Returns(headers);
+    return request.Object;
+  }
+
+  private ParticipantDto MockParticipant()
+  {
+    return new ParticipantDto
     {
-        var context = new Mock<FunctionContext>();
-        var request = new Mock<HttpRequestData>(MockBehavior.Strict, context.Object);
-        var headers = new HttpHeadersCollection(new List<KeyValuePair<string, string>>());
-        if (!string.IsNullOrEmpty(authHeader)) headers.Add("Authorization", $"{authHeader}");
-
-        request.Setup(r => r.Headers).Returns(headers);
-        return request.Object;
-    }
-
-    private ParticipantDto MockParticipant()
-    {
-        return new ParticipantDto
-        {
-            ParticipantId = _validParticipantId,
-            Name = "Test User",
-            NhsNumber = "12345678"
-        };
-    }
+      ParticipantId = _validParticipantId,
+      Name = "Test User",
+      NhsNumber = "12345678"
+    };
+  }
 }
