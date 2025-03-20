bebd867723dbf57652d50ece730a338076f8e360
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-03-20 13:28:48 +0000

    test: Added unit test

---
+++ tests/ParticipantManager.Shared.Tests/HostBuilderExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using ParticipantManager.Shared.Extensions;
+
+namespace ParticipantManager.Shared;
+
+public class HostBuilderExtensionsTests
+{
+  [Fact]
+  public void ConfigureSerligLogging_ShouldNotReturnNull()
+  {
+    // Arrange
+    var appInsightsConnectionString = "InstrumentationKey=value1";
+    var hostBuilder = new HostBuilder();
+
+    // Act
+    var configuredHostBuilder = hostBuilder.ConfigureSerilogLogging(appInsightsConnectionString);
+    configuredHostBuilder.Build();
+
+    // Assert
+    Assert.NotNull(configuredHostBuilder);
+  }
+}
