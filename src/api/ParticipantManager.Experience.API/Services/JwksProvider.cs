d8d946c95071b67418e89575a8220620cf0b79f2
warren <warren.pitterson1@nhs.net>
2025-03-28 14:33:11 +0000

    fix: removed redundant _logger

--- src/api/ParticipantManager.Experience.API/Services/JwksProvider.cs
+++ src/api/ParticipantManager.Experience.API/Services/JwksProvider.cs
@@ -12,13 +12,12 @@ public class JwksProvider(
 {
     private readonly IConfigurationManager<OpenIdConnectConfiguration> _configurationManager = configurationManager
     ?? new ConfigurationManager<OpenIdConnectConfiguration>($"{issuer}/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever(), new HttpDocumentRetriever());
-    private readonly ILogger<JwksProvider> _logger = logger;
 
     public async Task<IEnumerable<SecurityKey>> GetSigningKeysAsync()
     {
-        _logger.LogInformation("Fetching JWKS keys from OIDC discovery endpoint...");
+        logger.LogInformation("Fetching JWKS keys from OIDC discovery endpoint...");
         var config = await _configurationManager.GetConfigurationAsync(CancellationToken.None);
-        _logger.LogInformation("JWKS keys returned.");
+        logger.LogInformation("JWKS keys returned.");
         return config.SigningKeys;
     }
 }
