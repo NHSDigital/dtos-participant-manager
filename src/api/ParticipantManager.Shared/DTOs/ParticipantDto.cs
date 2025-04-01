6bf88f2c8c59042434c43ff3ba53df93a49042c1
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-04-01 15:53:01 +0100

    fix: Updated DTOs to reflect entity model changes

--- src/api/ParticipantManager.Shared/DTOs/ParticipantDto.cs
+++ src/api/ParticipantManager.Shared/DTOs/ParticipantDto.cs
@@ -3,7 +3,7 @@ namespace ParticipantManager.Shared.DTOs;
 public class ParticipantDto
 {
     public Guid ParticipantId { get; set; }
-    public DateTime DOB { get; set; }
+    public DateOnly DOB { get; set; }
     public required string NhsNumber { get; set; }
     public required string Name { get; set; }
 }
