3ca47967c6f9a3b2017c0447cfde2e460597db1e
Ian Nelson <ian@iannelson.systems>
2025-04-01 14:51:04 +0100

    fix: suppress hardcoded pathway url warning. Change fallback URL. (#118)

--- src/api/ParticipantManager.Shared/DTOs/PathwayInfoUrlMapper.cs
+++ src/api/ParticipantManager.Shared/DTOs/PathwayInfoUrlMapper.cs
@@ -2,15 +2,17 @@ namespace ParticipantManager.Shared.DTOs;
 
 public static class PathwayInfoUrlMapper
 {
+    #pragma warning disable S1075 // URIs should not be hardcoded
     private static readonly Dictionary<string, string> PathwayUrls = new()
     {
         { "Breast Screening Routine", "https://www.nhs.uk/conditions/breast-screening-mammogram/" },
         { "Cervical Screening Routine", "https://www.nhs.uk/conditions/cervical-screening/" },
         { "Bowel Screening Routine", "https://www.nhs.uk/conditions/bowel-cancer/" }
     };
+    #pragma warning restore S1075
 
     public static string GetUrl(string pathwayName)
     {
-        return PathwayUrls.TryGetValue(pathwayName, out var url) ? url : "https://example.com/default-screening";
+        return PathwayUrls.TryGetValue(pathwayName, out var url) ? url : "https://www.nhs.uk/conditions/nhs-screening/";
     }
 }
