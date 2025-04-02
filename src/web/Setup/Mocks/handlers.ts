c1b5b6e78a9d5530f18e9665c37793ef5dba053d
Tyrrellion <samuel.tyrell1@nhs.net>
2025-04-02 17:34:14 +0100

    feat: new way to skip login

--- src/web/Setup/Mocks/handlers.ts
+++ src/web/Setup/Mocks/handlers.ts
@@ -4,6 +4,11 @@ import accessToken from "../Data/access_token.json";
 
 export const handlers = [
   // Authentication handler (mocking NHS token exchange)
+  http.get("https://auth.sandpit.signin.nhs.uk/.well-known/openid-configuration", () => {
+    console.log("🟢 MSW handler hit: GitHub user endpoint");
+    return HttpResponse.json({"issuer": "https://auth.sandpit.signin.nhs.uk", "authorization_endpoint": "https://localhost:3000/api/auth/callback/nhs-login?code=fb66c0ac-38ec-4edb-b204-90623968a3e8", "token_endpoint": "https://auth.sandpit.signin.nhs.uk/token", "jwks_uri": "https://auth.sandpit.signin.nhs.uk/.well-known/jwks.json", "scopes_supported": ["basic_demographics", "client_metadata", "email", "gp_integration_credentials", "gp_registration_details", "landline", "nhs_app_credentials", "openid", "phone", "profile", "profile_extended"], "response_types_supported": ["code"], "subject_types_supported": ["public"], "id_token_signing_alg_values_supported": ["RS512"], "claims_supported": ["iss", "aud", "sub", "family_name", "given_name", "email", "email_verified", "phone_number", "phone_number_verified", "landline_number", "landline_number_verified", "birthdate", "nhs_number", "gp_integration_credentials", "delegations", "gp_registration_details", "im1_token", "auth_time", "jti", "nonce", "vot", "vtm", "exp", "ods_code", "user_id", "linkage_key", "surname", "iat", "gp_ods_code", "gp_user_id", "gp_linkage_key", "client_user_metadata", "identity_proofing_level"], "userinfo_endpoint": "https://auth.sandpit.signin.nhs.uk/userinfo", "token_endpoint_auth_signing_alg_values_supported": ["RS512"], "token_endpoint_auth_methods_supported": ["private_key_jwt"], "fido_uaf_authentication_request_endpoint": "https://uaf.sandpit.signin.nhs.uk/authRequest", "fido_uaf_registration_request_endpoint": "https://uaf.sandpit.signin.nhs.uk/regRequest", "fido_uaf_registration_response_endpoint": "https://uaf.sandpit.signin.nhs.uk/regResponse", "fido_uaf_deregistration_endpoint": "https://uaf.sandpit.signin.nhs.uk/dereg"}
+    );
+  }),
 
   http.post(
     "https://auth.sandpit.signin.nhs.uk/token",
--- src/web/app/components/signInButton.tsx
+++ src/web/app/components/signInButton.tsx
@@ -1,28 +1,8 @@
 import { getAuthConfig } from "@/app/lib/auth";
-import { cookies } from "next/headers";
 import styles from "@/app/styles/components/signIn.module.scss";
-import { redirect } from "next/navigation";
 
 async function handleSignIn() {
   "use server";
-
-  if (process.env.APP_ENV === "test") {
-    // 🔁 Simulate successful NHS login redirect in test
-    const mockVerifier = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiX0o2LWk1NC10d2ZLNEtZdmxIUkpxWEFnWHNjb1l2bVh5Z2xEbGFRMFF0cjQxdnZtNGhrMWU0UEFPTHN0dUZsak5GUXk2QnpYQUY0SDNqQU1VaXZQNkEifQ..P-PxAzfxtmNJdebFEujRyg.EP3rjItORQc-7kMihrPEUeC9D0WUaJLEknTorXZBrhL1Imik-HXKqJOHT4vDAjifu32tbuGIa5jS5hS6bbVZ1dvdX1DvRmRgdt_vHjHQKFJ8NYGDQ8CXphOewz0CAVKB06YtA_kBN9o1TIjlEVMw82vL6xadf8ztgVMiMnNkGL2pPUBIxMSHLnaenGSL3ssK.1Csy2Rpqevpav4JpsfPH0bfx1vpoCJkEFeAOSkkWRms";
-
-    const cookieStore = await cookies();
-    cookieStore.set("__Secure-authjs.pkce.code_verifier", mockVerifier, {
-      path: "/",
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax",
-    });
-
-    return redirect(
-      "https://localhost:3000/api/auth/callback/nhs-login?code=fb66c0ac-38ec-4edb-b204-90623968a3e8"
-    );
-  }
-
   const { signIn } = await getAuthConfig();
   await signIn("nhs-login", { redirectTo: "/screening" });
 }
