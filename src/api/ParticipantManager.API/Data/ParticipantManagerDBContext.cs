4f4355dffba00aa6ffd9fa817aa25830c477fe0e
Ian Nelson <ian@iannelson.systems>
2025-04-04 11:09:41 +0100

    remove encounter from DB Context

--- src/api/ParticipantManager.API/Data/ParticipantManagerDBContext.cs
+++ src/api/ParticipantManager.API/Data/ParticipantManagerDBContext.cs
@@ -3,17 +3,11 @@ using ParticipantManager.API.Models;
 
 namespace ParticipantManager.API.Data;
 
-public class ParticipantManagerDbContext : DbContext
+public class ParticipantManagerDbContext(DbContextOptions<ParticipantManagerDbContext> options) : DbContext(options)
 {
-    public ParticipantManagerDbContext(DbContextOptions<ParticipantManagerDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<Participant> Participants { get; set; }
     public DbSet<PathwayTypeEnrolment> PathwayTypeEnrolments { get; set; }
     public DbSet<Episode> Episodes { get; set; }
-    public DbSet<Encounter> Encounters { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -21,6 +15,5 @@ public class ParticipantManagerDbContext : DbContext
         modelBuilder.Entity<Participant>().HasKey(p => p.ParticipantId);
         modelBuilder.Entity<PathwayTypeEnrolment>().HasKey(pa => pa.EnrolmentId);
         modelBuilder.Entity<Episode>().HasKey(e => e.EpisodeId);
-        modelBuilder.Entity<Encounter>().HasKey(en => en.EncounterId);
     }
 }
