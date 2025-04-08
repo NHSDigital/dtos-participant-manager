f82a094796873db0dc0f733cb9d2766e27d5ca7b
Adam Chrimes <iChrimes@live.co.uk>
2025-04-08 17:59:27 +0100

    chore: fix the NEXT_PUBLIC_API_MOCKING check

--- src/web/app/layout.tsx
+++ src/web/app/layout.tsx
@@ -2,8 +2,8 @@ import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import "@/app/globals.scss";
 
-if (process.env.NEXT_PUBLIC_API_MOCKING == "enabled") {
-  require("../oidc-mock/mocks"); // Corrected path
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  require("../oidc-mock/mocks");
 }
 
 export default function RootLayout({
