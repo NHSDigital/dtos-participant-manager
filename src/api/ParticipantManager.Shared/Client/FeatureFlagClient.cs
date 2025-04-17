fdf09ae82c20d296077e414d6d5519d1ea2bf4e6
warren <warren.pitterson1@nhs.net>
2025-04-17 09:28:51 +0100

    feat: Added wrapper with default value to Feature Flag Client
    Signed-off-by: warren <warren.pitterson1@nhs.net>

--- src/api/ParticipantManager.Shared/Client/FeatureFlagClient.cs
+++ src/api/ParticipantManager.Shared/Client/FeatureFlagClient.cs
@@ -1,17 +1,27 @@
 using Flagsmith;
+using Microsoft.Extensions.Logging;
 
 namespace ParticipantManager.Shared.Client;
 
-public class FeatureFlagClient(IFlagsmithClient flagsmithClient) : IFeatureFlagClient
+public class FeatureFlagClient(IFlagsmithClient flagsmithClient, ILogger<FeatureFlagClient> logger) : IFeatureFlagClient
 {
     public async Task<bool> IsFeatureEnabledForParticipant(string featureName, Guid participantId)
     {
-        var identifier = participantId.ToString();
-        var traitKey = "participant_id";
-        var traitList = new List<ITrait> { new Trait(traitKey, participantId) };
+        var defaultValue = true;
 
-        var flags = await flagsmithClient.GetIdentityFlags(identifier, traitList);
+        try
+        {
+            var identifier = participantId.ToString();
+            var traitKey = "participant_id";
+            var traitList = new List<ITrait> { new Trait(traitKey, participantId) };
+            var flags = await flagsmithClient.GetIdentityFlags(identifier, traitList);
 
-        return await flags.IsFeatureEnabled(featureName);
+            return await flags.IsFeatureEnabled(featureName);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to fetch feature flag {FeatureName} for participant {ParticipantId}. Using default value {DefaultValue}.", featureName, participantId, defaultValue);
+            return defaultValue;
+        }
     }
 }
