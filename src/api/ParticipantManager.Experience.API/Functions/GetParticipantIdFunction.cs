1900f0644a83db482c21719f33db084bcacec402
LEE GATHERCOLE <lee.gathercole3@nhs.net>
2025-06-27 08:10:59 +0100

    Resolve sonar issues

--- src/api/ParticipantManager.Experience.API/Functions/GetParticipantIdFunction.cs
+++ src/api/ParticipantManager.Experience.API/Functions/GetParticipantIdFunction.cs
@@ -45,16 +45,6 @@ public class GetParticipantIdFunction(
                 return new NotFoundObjectResult("Unable to find participant");
             }
 
-            logger.LogInformation("Found participant for ParticipantId: " + participant.ParticipantId );
-
-            var enabled = await featureFlagClient.IsFeatureEnabledForParticipant("mays_mvp", participant.ParticipantId);
-
-            // if (!enabled)
-            // {
-            //     logger.LogInformation("Not enabled for ParticipantId: " + participant.ParticipantId );
-            //     return new ForbidResult();
-            // }
-
             logger.LogInformation("Found participant for NhsNumber: {@NhsNumber}",
                 new { NhsNumber = nhsNumber });
             return new OkObjectResult(participant.ParticipantId);
